ab0955ce938887d970e1ce019d3e3da809eb9171
vslavik <vslavik@0d8c8f3e-4bf7-0310-99c4-d70f6228f213>
2005-05-22 20:56:02 +0000

    ported KWQChar.mm to ICU

--- wxwebcore/trunk/webcore/kwq/KWQChar.cpp
+++ wxwebcore/trunk/webcore/kwq/KWQChar.cpp
@@ -1,4 +1,27 @@
 /*
+ * Copyright (c) 2005 Vaclav Slavik
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+/*
  * Copyright (C) 2004 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,45 +50,41 @@
 #import "KWQLogging.h"
 #import "WebCoreUnicode.h"
 
-#import <Foundation/Foundation.h>
+#include <unicode/uchar.h>
 
 bool QChar::isDigitNonASCII(UniChar c)
 {
-    static CFCharacterSetRef set = CFCharacterSetGetPredefined(kCFCharacterSetDecimalDigit);
-    return CFCharacterSetIsCharacterMember(set, c);
+    return u_isdigit(c);
 }
 
 bool QChar::isLetterNonASCII(UniChar c)
 {
-    static CFCharacterSetRef set = CFCharacterSetGetPredefined(kCFCharacterSetLetter);
-    return CFCharacterSetIsCharacterMember(set, c);
+    return u_isalpha(c);
 }
 
 bool QChar::isNumberNonASCII(UniChar c)
 {
-    return isLetterOrNumberNonASCII(c) && !isLetterNonASCII(c);
+    return u_isdigit(c);
 }
 
 bool QChar::isLetterOrNumberNonASCII(UniChar c)
 {
-    static CFCharacterSetRef set = CFCharacterSetGetPredefined(kCFCharacterSetAlphaNumeric);
-    return CFCharacterSetIsCharacterMember(set, c);
+    return u_isalnum(c);
 }
 
 bool QChar::isPunct() const
 {
-    static CFCharacterSetRef set = CFCharacterSetGetPredefined(kCFCharacterSetPunctuation);
-    return CFCharacterSetIsCharacterMember(set, c);
+    return u_ispunct(c);
 }
 
 UniChar QChar::lowerNonASCII(UniChar c)
 {
-    return WebCoreUnicodeLowerFunction(c);
+    return WebCoreUnicodeLowerFunction(c); // FIXME: use u_tolower
 }
 
 UniChar QChar::upperNonASCII(UniChar c)
 {
-    return WebCoreUnicodeUpperFunction(c);
+    return WebCoreUnicodeUpperFunction(c); // FIXME: use u_toupper
 }
 
 bool QChar::mirrored() const
