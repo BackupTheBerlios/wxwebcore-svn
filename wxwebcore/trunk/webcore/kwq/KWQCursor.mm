f68427dbd85f77abe09b01ed3536f4edef07f98a
kollivier <kollivier@0d8c8f3e-4bf7-0310-99c4-d70f6228f213>
2005-05-26 06:31:30 +0000

    wx implementation.

--- wxwebcore/trunk/webcore/kwq/KWQCursor.mm
+++ wxwebcore/trunk/webcore/kwq/KWQCursor.mm
@@ -1,4 +1,27 @@
 /*
+ * Copyright (c) 2005 Kevin Ollivier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+/*
  * Copyright (C) 2004 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,46 +48,45 @@
 
 #import "KWQCursor.h"
 #import "KWQLogging.h"
-#import "KWQFoundationExtras.h"
 
 // The NSCursor cocoa calls here can't fail, so no need to block Cocoa exceptions
 
 QCursor::QCursor()
-    : cursor(nil)
+    : cursor(NULL)
 {
 }
 
-QCursor::QCursor(NSCursor *cur)
-    : cursor(KWQRetain(cur))
+QCursor::QCursor(const wxCursor &cur)
+    : cursor(cur)
 {
 }
 
 QCursor::QCursor(const QPixmap &pixmap)
-    : cursor(nil)
+    : cursor(NULL)
 {
     // Needed for custom cursors.
     ERROR("not yet implemented");
 }
 
 QCursor::QCursor(const QCursor &other)
-    : cursor(KWQRetain(other.cursor))
+    : cursor(other.cursor)
 {
 }
 
 QCursor::~QCursor()
 {
-    KWQRelease(cursor);
+	cursor = NULL;
 }
       
 QCursor &QCursor::operator=(const QCursor &other)
 {
-    KWQRetain(other.cursor);
-    KWQRelease(cursor);
+	// wx ref-counts wxCursor
+	cursor = NULL;
     cursor = other.cursor;
     return *this;
 }
 
-NSCursor *QCursor::handle() const
+wxCursor QCursor::handle() const
 {
     return cursor;
 }
